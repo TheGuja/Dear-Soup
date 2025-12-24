cd38c37f024d4e2b9dca97495ed484ba1528ac10
Eric Gu <ejgu@umass.edu>
2025-12-23 20:05:28 -0500

    cleaned up commented out code lines

--- utils/utils.ts
+++ utils/utils.ts
@@ -10,14 +10,9 @@ export async function getCurrentUser(supabase: SupabaseClient) {
     if (error) {
         throw error;
     }
-
-    // console.log(user?.id);
+    
     return user?.id;
 };
-
-// export async function getCurrentJournalID(supabase: SupabaseClient) {
-    
-// }
 
 export async function saveJournal(sharedUser: string, title: string, journalID: string): Promise<void> {
     // Check handling of empty string for sharedUser, title, and content
@@ -43,11 +38,6 @@ export async function savePage(journalID: string, date: Date, content: string): 
     // Check handling of empty string for sharedUser, title, and content
     const supabase = await createClient();
     const currentUserID = await getCurrentUser(supabase);
-    // console.log("Fetching");
-    // console.log(currentUserID);
-    // const [currentUserID, sharedUserID] = await Promise.all([getCurrentUser(supabase), supabase.from("users").select("id").eq("email", sharedUser).single()]);
-    // const sharedUserID = await supabase.from('users').select('id').eq('email', sharedUser).single()
-
     const { error } = await supabase.from('pages').upsert({user_id: currentUserID, journal_id: journalID, created_at: date, page_content: content}, {onConflict: 'journal_id, user_id, created_at'})
 
     if (error) {
@@ -57,29 +47,16 @@ export async function savePage(journalID: string, date: Date, content: string): 
 
 export async function loadPage(journalID: string, date: Date): Promise<string[]> {
     const supabase = await createClient();
-    // const currentUserID = await getCurrentUser(supabase);
     const EMPTY_STATE = '{"root":{"children":[{"children":[],"direction":null,"format":"","indent":0,"type":"paragraph","version":1}],"direction":null,"format":"","indent":0,"type":"root","version":1}}';
     const formattedDate = new Intl.DateTimeFormat('en-US').format(date);
 
     const toReturn: string[] = [EMPTY_STATE, EMPTY_STATE];
-    const [currentUserID, content] = await Promise.all([getCurrentUser(supabase), supabase.from('pages').select('page_content, user_id').match({ 'journal_id': journalID, 'created_at': formattedDate})])
-    // console.log(content);
-    // toReturn[0] = "a"
-    // console.log(toReturn)
-
-    // if (content.error) {
-    //     throw content.error;
-    // } else {
-    //     if (content.data.length == 0) {
-    //         return [EMPTY_STATE, EMPTY_STATE];
-    //     }
-    // };
+    const [currentUserID, content] = await Promise.all([getCurrentUser(supabase), supabase.from('pages').select('page_content, user_id').match({ 'journal_id': journalID, 'created_at': formattedDate})]);
 
     if (content.error) {
         throw content.error;
     } else {
         content.data.forEach((entry) => {
-            // console.log(entry.page_content);
             if (entry.user_id == currentUserID) {
                 toReturn[0] = entry.page_content;
             } else {
@@ -89,65 +66,4 @@ export async function loadPage(journalID: string, date: Date): Promise<string[]>
     };
 
     return toReturn;
-
-    // const currentUserID = getCurrentUser(supabase)
-    // const getOtherUser = await supabase.from('journals').select('other_id').eq('journal_id', journalID).single()
-
-    // const userIDs = await Promise.all([getCurrentUser(supabase), supabase.from('journals').select('other_id').eq('journal_id', journalID).single()]);
-    // if (getOtherUser.error) {
-    //     throw getOtherUser.error;
-    // } else {
-    // const { data, error } = await supabase.from('pages').select('page_content, user_id').match({ 'journal_id': journalID, 'created_at': formattedDate }).in('user_id', userIDs);
-
-    // if (error) {
-    //     throw error;
-    // };
-
-    // console.log(data);
-
-
-    // const contentMap = data.reduce((acc, row) => {
-    //     acc[row.user_id] = row.page_content;
-    //     return acc;
-    // }, {});
-    
-    // let otherUserID = "";
-    // if (otherUser.error) {
-    //     throw otherUser.error;
-    // } else {
-    //     otherUserID = otherUser.data.other_id;
-    // };
-
-    // };
-
-    // const { data, error } = await supabase.from('pages').select('page_content, user_id').match({ 'journal_id': journalID, 'created_at': formattedDate }).in('user_id', userIDs);
-    // const otherUserID = supabase.from('journals').select('other_id')
-    // console.log(data);
-
-    // const [currentUserPage, otherUserPage] = await Promise.all([supabase.from('pages').select('page_content').match({ 'journal_id': journalID, 'created_at': formattedDate, 'user_id': currentUserID}).single(), supabase.from('pages').select('page_content').match({ 'journal_id': journalID, 'created_at': formattedDate, 'user_id': otherUserID}).single()]);
-    // console.log(currentUserPage);
-    // console.log(otherUserPage);
-    // await supabase.from('pages').select('page_content').match({ 'journal_id': journalID, 'created_at': formattedDate, 'user_id': currentUserID});
-    // await supabase.from('pages').select('page_content').match({ 'journal_id': journalID, 'created_at': formattedDate, 'user_id': otherUserID});
-
-
-    
-    
-    // const res = await supabase.from('pages').select('page_content').match({'journal_id': journalID, 'created_at': formattedDate});
-    // console.log(res.data?.at(0)?.page_content);
-    // if (res.count == null) {
-    //     console.log("none")
-    // }
-    // console.log(res.data?.length)
-
-    // if (res.error) {
-    //     throw res.error;
-    // } else if (res.data?.length == 0) {
-    //     return EMPTY_STATE;
-    // } else {
-    //     const content = res.data?.at(0)?.page_content;
-    //     // console.log(res);
-    //     // console.log(content);
-    //     return content;
-    // };
 };
